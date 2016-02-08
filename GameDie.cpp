9425ac7380023f86d9826e9657859965779b02d1
Sumeet Tulsani <sumeettulsani@wlan-t-255-105-dhcp.CSUChico.EDU>
2016-02-08 10:44:33 -0800

    GameDie producing number between x and 20 when rolled, where x is number passed from main.cpp

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,11 +1,22 @@
 #include <cstdlib>
+#include <time.h>
 
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+	srand(time(NULL));     //seed the random number generator globally
 }
 
 //generate a random number between 1-6 (inclusive) and return it
 int GameDie::roll()
 {
+	return 1;
+}
+
+
+int GameDie::roll(int x)
+{
+	int y = 20 - x;
+	int num = rand() % x + y;      //using rand() to generate random number 						   between x and 20 inclusive          
+    	return num;
 }
